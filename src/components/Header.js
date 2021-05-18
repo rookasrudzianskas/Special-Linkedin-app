8051b24225329c2fb306d359decc3d8520ef1607
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 15:32:41 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,58 @@ import styled from "styled-components";
 
 const Header = () => {
     return (
-        <div>
-Header
-        </div>
+        <Container>
+            <Content>
+                <Logo>
+                    <a href="/home">
+                        <img src="/images/home-logo.svg" alt=""/>
+                    </a>
+                </Logo>
+
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search"/>
+                    </div>
+                    <SearchIcon>
+                        <img src="/images/search-icon.svg" alt=""/>
+                    </SearchIcon>
+                </Search>
+            </Content>
+        </Container>
     );
 };
 
 export default Header;
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+  
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+  
+`;
+
+const Logo = styled.span`
+  margin-right: 8px;
+  font-size: 0px;
+`;
+
+const Search = styled.div`
+
+`;
+
+const SearchIcon= styled.div`
+
+`;
