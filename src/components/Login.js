31ff3f6b919e9020ded9befbf0e98b33e1ac99cd
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 14:43:34 +0300

    Rokas

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,10 @@ const Login = () => {
                 <a href="/">
                     <img src="/images/login-logo.svg" alt=""/>
                 </a>
+
+                <div>
+                    <Join>Join now</Join>
+                </div>
             </Nav>
         </Container>
     );
@@ -15,5 +19,44 @@ const Login = () => {
 
 export default Login;
 
-const Container = styled.div``;
-const Nav = styled.div``;
+const Container = styled.div`
+  padding: 0px;
+`;
+const Nav = styled.div`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0px 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  
+  & > a {
+    width: 135px;
+    height: 34px;
+    
+    
+    @media(max-width: 768px) {
+      padding: 0 5px;
+      
+    }
+  }
+`;
+
+const Join = styled.div`
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  border-radius: 4px;
+  
+  
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0,0 ,0 , 0.9);
+    text-decoration: none;
+    
+  }
+`
