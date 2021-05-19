191ad61f88ae5db080333bf683c7dc40596b85c8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 07:59:03 +0300

    Rokas

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -5,7 +5,35 @@ const RightSide = () => {
     return (
         <Container>
             <FollowCard>
-                rightside
+
+                <Title>
+                    <h2>Add to your feed</h2>
+                    <img src="/images/feed-icon.svg" alt="" />
+                </Title>
+
+                <FeedList>
+                    <li>
+                        <a>
+                            <Avatar />
+                        </a>
+
+                        <div>
+                            <span>#Linkedin</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+
+                    <li>
+                        <a>
+                            <Avatar />
+                        </a>
+
+                        <div>
+                            <span>#Video</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+                </FeedList>
             </FollowCard>
         </Container>
     );
@@ -25,4 +53,57 @@ const FollowCard = styled.div`
   margin-bottom: 8px;
   background-color: #fff;
   border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
+`
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
+  
+`
+
+const FeedList = styled.ul`
+  margin-top: 16px;
+  
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
+    position: relative;
+    font-size: 14px;
+    
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+    
+    button {
+      background-color: transparent;
+      color: rgba(0, 0, 0, 0.6);
+      box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      align-items: center;
+      border-radius: 15px;
+      box-sizing: border-box;
+      font-weight: 600;
+      display: inline-flex;
+      justify-content: center;
+      max-height: 32px;
+      max-width: 480px;
+      text-align: center;
+      outline: none;
+      
+    }
+  }
+`
+
+const Avatar = styled.div`
+  
 `
