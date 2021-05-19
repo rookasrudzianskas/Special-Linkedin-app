c9f0f16395c65c8d9dbe31ea01844928b35d2239
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 06:48:27 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -57,7 +57,28 @@ const Header = () => {
                             </a>
                         </NavList>
 
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt=""/>
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg" alt=""/>
+                            </a>
 
+                            <SignOut>
+                                <a>
+                                    SignOut
+                                </a>
+                            </SignOut>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt=""/>
+                                <span>Work
+                                    <img src="/images/down-icon.svg" alt=""/>
+                                </span>
+                            </a>
+                        </Work>
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -204,4 +225,52 @@ const NavList = styled.li`
       }
     }
   }
+  `
+
+const SignOut = styled.div`
+ position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
 `
+
+
+const User = styled(NavList)`
+  a > svg {
+    width: 24px;
+    border-radius: 50%;
+    
+  }
+  
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+  
+  span {
+    display: flex;
+    align-items: center;
+  }
+  
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`
+
+
+const Work = styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.08);
+`
+
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,28 @@ import React from 'react';
 import styled from "styled-components";
 const Home = () => {
     return (
-        <div>
-Home
-        </div>
+        <Container>
+            <Section>
+                <h5>
+                    <a>
+                        Hiring in a hurry? -
+                    </a>
+                </h5>
+                <p>Find talented pros in record time with Upwork and keep business
+                    moving.
+                </p>
+            </Section>
+        </Container>
     );
 };
 
 export default Home;
+
+const Container = styled.div`
+  padding-top: 76px;
+  max-width: 100%;
+  
+`;
+
+const Content = styled.div``;
+const Section = styled.section``;
