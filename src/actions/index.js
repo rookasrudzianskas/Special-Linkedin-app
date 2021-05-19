68f7e18956ff4c9525afe3a35e5895a4682c19c4
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 12:21:36 +0300

    Rokas

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import {auth, provider } from "../firebase";
 // logins the user to the google
 export function signInAPI() {
     return(dispatch) => {
+        // then it logins, it gets the object, which we call payload, and we pass to the forward, and log on the screen
         auth.signInWithPopup(provider).then(payload => {
             console.log(payload);
         }).catch(error => alert(error));
