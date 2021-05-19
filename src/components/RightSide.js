a849d7958cd5b0b04ecad149234ddb394e1b6c1d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 07:43:19 +0300

    Rokas

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -4,7 +4,9 @@ import styled from "styled-components";
 const RightSide = () => {
     return (
         <Container>
-            Right side
+            <FollowCard>
+                rightside
+            </FollowCard>
         </Container>
     );
 };
@@ -15,5 +17,12 @@ export default RightSide;
 
 const Container = styled.div`
   grid-area: rightside;
-  
+`
+
+const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
 `
