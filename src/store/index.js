226107ff7385da3b4d8dccba5344a0d4ecd5f812
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 12:19:05 +0300

    Rokas

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import thunkModdleware from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 
 import rootReducer from "../reducers";
 
 // this creates a data layer to store all the things, we are accessing this data layer
-const store = createStore(rootReducer, applyMiddleware(thunkModdleware));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export default store;
